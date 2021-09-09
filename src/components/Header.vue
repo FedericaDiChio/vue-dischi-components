af3a032ec8b3609ff58077719768fcd823e230db
FedericaDiChio <federica.dichio7@gmail.com>
2021-09-09 15:03:32 +0200

    header

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+    <header>
+        <div class="container">
+          <img src="" />
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header",
+};
+</script>
+
+<style lang="scss">
+header {
+  background-color: #2e3a46;
+  height: 70px;
+  padding: 10px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+//   header img {
+//     height: 50px;
+//   }
+}
+</style>
