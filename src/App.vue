1455f0303ece3f1442859bc28d4ee9ebc4046700
FedericaDiChio <federica.dichio7@gmail.com>
2021-09-09 15:54:05 +0200

    axios e card components

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <template>
   <div id="app">
     <Header />
+    <Products />
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
+import Products from './components/Products.vue'
 
 export default {
   name: 'App',
   components: {
     Header,
+    Products,
   }
 }
 </script>
 
 <style lang="scss">
-
+ @import "./assets/styles/style.scss";
 </style>
---
+++ src/components/Products.vue
@@ -0,0 +1,37 @@
+<template>
+  <main class="container">
+    <section id="album-container">
+        <ProductCards v-for="(album, index) in albums" :key="index" :album="album" />
+    </section>
+  </main>
+</template>
+
+<script >
+import axios from "axios";
+import ProductCards from "./ProductCards.vue";
+export default {
+  components: { ProductCards },
+  name: "Products",
+  data() {
+    return {
+      albums: [],
+    };
+  },
+  created() {
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/array/music")
+      .then((res) => {
+        this.albums = res.data.response;
+      });
+  },
+};
+</script>
+
+<style lang="scss">
+#album-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+
+}
+</style>
